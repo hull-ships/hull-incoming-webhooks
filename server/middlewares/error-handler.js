baf352fff748e589ff5d6148eab76bc28e060086
Stephane Bellity <stephane@hull.io>
2017-08-01 19:26:21 +0200

    Fix lint error + user-update handler

--- server/middlewares/error-handler.js
+++ server/middlewares/error-handler.js
@@ -14,8 +14,6 @@ export default function (app: express) {
         url: req.url,
         params: req.params
       };
-
-
       const logger = _.get(req, "hull.client.logger");
       if (logger) {
         logger.error("request.error", err.message, err.status, data);
@@ -24,7 +22,6 @@ export default function (app: express) {
       }
       return res.status(_.get(err, "status") || 500).send({ message: _.get(err, "message") });
     }
-    
     Hull.logger.warn("Unknown error ?");
     return res.status(500).send({ message: "Unknown error" });
   });
