8704417ba1f1f090b7a9358239a46011e58f4ef2
Stephane Bellity <stephane@hull.io>
2017-08-01 11:57:04 +0200

    lint

--- server/actions/webhook-handler.js
+++ server/actions/webhook-handler.js
@@ -3,13 +3,15 @@ import { Request, Response } from "express";
 import updateUser from "../user-update";
 import _ from "lodash";
 
-export default function webhookHandler(req: Request, res: Response) {
-  res.send();
-  return req.hull.cache.set("webhookRequest", pickValuesFromRequest(req), { ttl: 1440000000 }).then((cachedValue) => {
-    updateUser(cachedValue, req.hull)
-  });
-}
-
 function pickValuesFromRequest(req: Request) {
   return _.pick(req, ["body", "headers", "cookies", "ip", "method", "params", "query"]);
+}
+
+export default function webhookHandler(req: Request, res: Response) {
+  res.send();
+  const ttl = 1440000000;
+  const payload = pickValuesFromRequest(req);
+  return req.hull.cache
+    .set("webhookRequest", payload, { ttl })
+    .then(cachedValue => updateUser(cachedValue, req.hull));
 }
