7c0ebd48d024fa6f71c26e7671705018d557cede
Michael Lefebvre <michael@scenedata.com>
2018-08-09 15:43:36 +0200

    remove old files

--- src/ui/header.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default ({title, children}) => (
-  <div className="flexRow">
-    <h5 className="mb-0 mt-0 text-muted flexGrow">{title}</h5>
-    {children}
-  </div>
-)
