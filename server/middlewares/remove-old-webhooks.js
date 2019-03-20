b1ba0722f17be62f25cdc75d0bfe42b56d5cd783
Tim Liu <tim@hull.io>
2019-03-20 11:21:03 +0100

    fixed lint problems

--- server/middlewares/remove-old-webhooks.js
+++ server/middlewares/remove-old-webhooks.js
@@ -1,7 +1,5 @@
 /* @flow */
 import { Request, Response } from "express";
-import moment from "moment";
-import _ from "lodash";
 
 export default function removeOldWebhooks(WebhookModel: Object) {
   return (req: Request, res: Response) => {
@@ -12,18 +10,16 @@ export default function removeOldWebhooks(WebhookModel: Object) {
       if (err) {
         client.logger.debug("mongo.query.error", { errors: err });
         res.status(500).json({});
-      }
-
-      // docs should exist and be equal to or greater (shoudn't happen)
-      // than 100, otherwise we don't need to trim
-      if (docs && docs.length >= 100) {
+      } else if (docs && docs.length >= 100) {
+        // docs should exist and be equal to or greater (shoudn't happen)
+        // than 100, otherwise we don't need to trim
         const lastWebhook = docs[docs.length - 1];
-        const removeOldWebhooks = WebhookModel.remove({ connectorId: ship.id, date: { $lt: lastWebhook.date } });
-        removeOldWebhooks.lean().exec((err, docs) => {
-          return res.status(200).json({ result: "groomed" });
+        const removeOldWebhooksQuery = WebhookModel.remove({ connectorId: ship.id, date: { $lt: lastWebhook.date } });
+        removeOldWebhooksQuery.lean().exec(() => {
+          res.status(200).json({ result: "groomed" });
         });
       } else {
-        return res.status(200).json( { result: "not enough webhooks to groom" });
+        res.status(200).json({ result: "not enough webhooks to groom" });
       }
     });
   };
