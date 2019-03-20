0d2f7bed3427ee88a50e7ebca8b7538e771d9b3a
Tim Liu <tim@hull.io>
2019-03-20 11:08:22 +0100

    upped limit to 100

--- server/middlewares/remove-old-webhooks.js
+++ server/middlewares/remove-old-webhooks.js
@@ -6,7 +6,7 @@ import _ from "lodash";
 export default function removeOldWebhooks(WebhookModel: Object) {
   return (req: Request, res: Response) => {
     const { client, ship = {} } = req.hull;
-    const query = WebhookModel.find({ connectorId: ship.id }).sort({ date: -1 }).limit(5);
+    const query = WebhookModel.find({ connectorId: ship.id }).sort({ date: -1 }).limit(100);
 
     query.lean().exec((err, docs) => {
       if (err) {
@@ -16,7 +16,7 @@ export default function removeOldWebhooks(WebhookModel: Object) {
 
       // docs should exist and be equal to or greater (shoudn't happen)
       // than 100, otherwise we don't need to trim
-      if (docs && docs.length >= 5) {
+      if (docs && docs.length >= 100) {
         const lastWebhook = docs[docs.length - 1];
         const removeOldWebhooks = WebhookModel.remove({ connectorId: ship.id, date: { $lt: lastWebhook.date } });
         removeOldWebhooks.lean().exec((err, docs) => {
