1c75102037ca7b415d03f47a061566c6eb6601cf
andyh565 <48692643+andyh565@users.noreply.github.com>
2021-02-23 10:41:41 -0500

    Update remove-old-webhooks.js

--- server/middlewares/remove-old-webhooks.js
+++ server/middlewares/remove-old-webhooks.js
@@ -4,13 +4,13 @@ import { Request, Response } from "express";
 export default function removeOldWebhooks(WebhookModel: Object) {
   return (req: Request, res: Response) => {
     const { client, ship = {} } = req.hull;
-    const query = WebhookModel.find({ connectorId: ship.id }).sort({ date: -1 }).limit(10);
+    const query = WebhookModel.find({ connectorId: ship.id }).sort({ date: -1 }).limit(50);
 
     query.lean().exec((err, docs) => {
       if (err) {
         client.logger.debug("mongo.query.error", { errors: err });
         res.status(500).json({});
-      } else if (docs && docs.length >= 10) {
+      } else if (docs && docs.length >= 50) {
         // docs should exist and be equal to or greater (shoudn't happen)
         // than 100, otherwise we don't need to trim
         const lastWebhook = docs[docs.length - 1];
